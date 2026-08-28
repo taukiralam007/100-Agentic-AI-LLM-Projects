bd6b0ccd30fc1cfdb0c52828d06fbddbc814a1e5
taukir.alam007@gmail.com <taukir.alam007@gmail.com>
2026-08-28 19:57:19 +0800

    Normalize calculator operation input

--- projects/01-scholar-agent/agent.py
+++ projects/01-scholar-agent/agent.py
@@ -2,20 +2,20 @@ from tools import calculator, word_counter, text_uppercase
 def run_agent(command):
      command = command.strip().lower() 
 
-     if command == "uppercase": 
+     if  "uppercase" in command: 
           text = input("Enter the text:") 
           return text_uppercase(text)
 
-     elif command == "count":
+     elif "count" in command:
           text = input("Enter text:") 
           return word_counter(text) 
 
-     elif command == "calculator":
+     elif  "calculator" in command or "calculate" in command:
           a = float(input("Enter the first number:")) 
           b = float(input("Enter the second number:")) 
           operation = input(
                               "Enter operation (add/substract/multiply/devide): "
-                            ) 
+                            ).strip().lower() 
           return calculator(a,b,operation) 
      else: 
           return "I don't know which tools to use" 
